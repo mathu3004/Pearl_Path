2e0ccb03f69053a88b256c68f1dd82bea290bb53
mathu3004 <160145869+mathu3004@users.noreply.github.com>
2025-03-01 16:33:53 +0000

    Itinerary Visualization Page

--- Design_Travel_Itinerary/travel-itinerary-generator/src/App.js
+++ Design_Travel_Itinerary/travel-itinerary-generator/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TravelItineraryGenerator from './TravelItineraryGenerator';
 import VisualizationItinerary from './VisualizationItinerary';
+import Pearl from './Pearl'
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<TravelItineraryGenerator />} />
+            <Route path="/" element={<Pearl />} />
+            <Route path="/" element={<TravelItineraryGenerator />} />
                 <Route path="/visualization" element={<VisualizationItinerary />} />
             </Routes>
         </Router>
